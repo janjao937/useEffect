148d132cd5d20df55a7f1f2a60c0e962b6a1a740
janjao937 <janjao937@gmail.com>
2023-09-06 14:59:02 +0700

    setup css

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const App =()=>{
+  return(
+    <div className="App">
+      <div className="Header">
 
+      <h1>Movie App</h1>
+      </div>
+      <div className="Search">
+        <input type="text" placeholder="Keyword" />
+        <button> Search</button>
+      </div>
+
+      <div className="Movie-list">
+          <div className="Movie">Banana</div>
+          <div className="Movie">Banana</div>
+      </div>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  
-  </React.StrictMode>
+  <App/>
 );
 
