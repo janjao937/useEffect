702cb883835bfe419f63aa7ec4185fa5de6fd08e
janjao937 <janjao937@gmail.com>
2023-09-06 23:17:07 +0700

    change page

--- src/index.js
+++ src/index.js
@@ -4,74 +4,107 @@ import { useState } from 'react';
 import './index.css';
 
 const BaseURL = "https://api.themoviedb.org/3";
-const Token ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGFmNjQ2N2JlNzA2ZTRiNzI0YzZjZmUwNmQwMmUyZiIsInN1YiI6IjY0ZjgxNzhmNWYyYjhkMDEzOWQwMGJmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CdmbiR_pCCgyM8uQ9GrqMy9PMKlKAqO07ySRtL4I1vc";
+const Token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGFmNjQ2N2JlNzA2ZTRiNzI0YzZjZmUwNmQwMmUyZiIsInN1YiI6IjY0ZjgxNzhmNWYyYjhkMDEzOWQwMGJmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CdmbiR_pCCgyM8uQ9GrqMy9PMKlKAqO07ySRtL4I1vc";
 
-const App =()=>{
-  const [Keyword,setKeyword] = useState("");
-  const [movieList,setMovieList] = useState([]);
-  const [allPage,setAllPage] = useState(0);
+const App = () => {
+  const [Keyword, setKeyword] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [allPage, setAllPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const OnChangeHandler = (e)=>{
+
+
+  const OnChangeHandler = (e) => {
     setKeyword(e.target.value);
-   
+
   }
 
-  const OnSubmitHandeler = async (e)=>{
+  const OnSubmitHandeler = async (e) => {
     e.preventDefault();
 
-    let url =`${BaseURL}/search/keyword?query=${Keyword}&page=1`;
-    let option ={
-      method:"GET",
-      headers:{
-        accept:"application/json",
-        Authorization :`Bearer ${Token}`,
+    let url = `${BaseURL}/search/keyword?query=${Keyword}&page=${currentPage}`;
+    let option =
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${Token}`,
       }
-  };
-  
-  try{
-    const response = await fetch(url,option);
-    const data = await response.json();
-    setMovieList(data.results);
-    setAllPage(data.total_pages);
-    // const movie = data.results;
-    
-  }catch(error){
-    console.log(error)
+    };
+
+    try {
+      const response = await fetch(url, option);
+      const data = await response.json();
+      setMovieList(data.results);
+      setAllPage(data.total_pages);
+      // const movie = data.results;
+
+    } catch (error) {
+      console.log(error)
+    }
+
   }
-  
 
-}
+  const OnChangePageHandeler = async ({page }) => {
+   
+    
+    let url = `${BaseURL}/search/keyword?query=${Keyword}&page=${page}`;
+    let option =
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${Token}`,
+      }
+    };
 
-  return(
+    try {
+      const response = await fetch(url, option);
+      const data = await response.json();
+      // const movie = data.results;
+      
+      setMovieList(data.results);
+      setAllPage(data.total_pages);
+      setCurrentPage(page);
+
+    }
+    catch (error) {
+      console.log(error)
+    } 
+  }
+
+  return (
     <div className="App">
       <div className="Header">
         <h1>Movie App</h1>
       </div>
-      <form  onSubmit={OnSubmitHandeler} className="Search">
-        <input onChange={(e)=>OnChangeHandler(e)} type="text" placeholder="Keyword" value={Keyword} />
+      <form onSubmit={OnSubmitHandeler} className="Search">
+        <input onChange={(e) => OnChangeHandler(e)} type="text" placeholder="Keyword" value={Keyword} />
         <button type="submit"> Search</button>
       </form>
+      
       <div>
-
-        {Array.from(Array(allPage).keys()).map((n,index) => (
-          <button key={index}>{n + 1}</button>
+        <p>{currentPage}</p>
+        {Array.from(Array(allPage).keys()).map((n) => (
+          <button  onClick={() => OnChangePageHandeler({ page: n + 1 })} key={n + 1}>{n + 1}</button>
         ))}
-        
+
       </div>
       <div className="Movie-list">
-         
-        {movieList.map(e=><div key={e.id} className="Movie">{e.name }</div> )}
-        
-         
+
+        {movieList.map(e => <div key={e.id} className="Movie">{e.name}</div>)}
+
+
       </div>
     </div>
-  );}
+  );
+}
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <App />
 );
 
