a9a68e3cd30321bb53949a1ee54fbf7db6ee65a5
janjao937 <janjao937@gmail.com>
2023-09-06 16:29:15 +0700

    page movie app

--- src/index.js
+++ src/index.js
@@ -1,26 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 import './index.css';
 
+const BaseURL = "https://api.themoviedb.org/3";
+const Token ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGFmNjQ2N2JlNzA2ZTRiNzI0YzZjZmUwNmQwMmUyZiIsInN1YiI6IjY0ZjgxNzhmNWYyYjhkMDEzOWQwMGJmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CdmbiR_pCCgyM8uQ9GrqMy9PMKlKAqO07ySRtL4I1vc";
+
 const App =()=>{
+  const [Keyword,setKeyword] = useState("");
+  const [movieList,setMovieList] = useState([]);
+  const [allPage,setAllPage] = useState(1);
+
+  const OnChangeHandler = (e)=>{
+    setKeyword(e.target.value);
+   
+  }
+
+  const OnSubmitHandeler = async (e)=>{
+    e.preventDefault();
+
+    let url =`${BaseURL}/search/keyword?query=${Keyword}&page=1`;
+    let option ={
+      method:"GET",
+      headers:{
+        accept:"application/json",
+        Authorization :`Bearer ${Token}`,
+      }
+  };
+  
+  try{
+    const response = await fetch(url,option);
+    const data = await response.json();
+    setMovieList(data.results);
+    setAllPage(data.total_pages);
+    // const movie = data.results;
+    
+  }catch(error){
+    console.log(error)
+  }
+  
+
+}
+
   return(
     <div className="App">
       <div className="Header">
-
-      <h1>Movie App</h1>
+        <h1>Movie App</h1>
       </div>
-      <div className="Search">
-        <input type="text" placeholder="Keyword" />
-        <button> Search</button>
+      <form  onSubmit={OnSubmitHandeler} className="Search">
+        <input onChange={(e)=>OnChangeHandler(e)} type="text" placeholder="Keyword" value={Keyword} />
+        <button type="submit"> Search</button>
+      </form>
+      <div>
+           
       </div>
-
       <div className="Movie-list">
-          <div className="Movie">Banana</div>
-          <div className="Movie">Banana</div>
+         
+        {movieList.map(e=><div key={e.id} className="Movie">{e.name }</div> )}
+        
+         
       </div>
     </div>
-  );
-}
+  );}
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App/>
