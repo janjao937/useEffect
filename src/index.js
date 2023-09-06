21fea6a219d6c853f050944d4935973b0da348b8
janjao937 <janjao937@gmail.com>
2023-09-06 17:33:27 +0700

    add useState

--- src/index.js
+++ src/index.js
@@ -8,25 +8,54 @@ const App = ()=>{
   const [count,setCount] =useState(0);
   //useEffect(()=>{},[]) //function,dependency array
   // useEffect(()=>console.log("Banana Effect"));//ทำงานหลัง Render & Re-render
-  useEffect(()=>console.log("Banana Effect"),[category]);//ทำงานหลัง Render & Re-render ตอนตัวแปร state ที่เราใส่ใน Array (ในนี้จะทำงานตอน category เปลี่ยน)
+  useEffect(()=>{console.log("First render")},[]);//จะ render แค่หลังครั้งแรก
+  useEffect(()=>{
+    
+    const fetchList = async ()=>{
+      const BaseUrl ="https://jsonplaceholder.typicode.com"
+      try{
+        let response = await fetch(`${BaseUrl}/${category}`,
+        {
+          method:"GET",
+        });
+        let data = await response.json();
+        console.log(data);
+
+      }catch(error){
+        console.log(error);
+      }
+    }
+
+
+   if(category !== "") fetchList();    //Call function
+
+  },[category]);//ทำงานหลัง Render & Re-render ตอนตัวแปร state ที่เราใส่ใน Array (ในนี้จะทำงานตอน category เปลี่ยน) คล้ายๆ delegate 
+
   useEffect(()=>console.log("Count"),[count]);
+  useEffect(()=>console.log("Count and Category"),[count,category]);
+
   console.log("Render");
   return(
     <div>
       
         <h1>useEffect:{category||"Not select"}</h1>
       <div className="button__group">
-        <button onClick={()=>setCategoty("Post")}>Post</button>
-        <button onClick={()=>setCategoty("Photo")}>Photo</button>
-        <button onClick={()=>setCategoty("TODO")}>Todo</button>
-        <button onClick={()=>setCategoty("User")}>Users</button>
-      </div>
-      <ul className="list">
-        <li className="list-item">item-1</li>
-      </ul>
+        <button onClick={()=>setCategoty("posts")}>Posts</button>
+        <button onClick={()=>setCategoty("photos")}>Photos</button>
+        <button onClick={()=>setCategoty("todos")}>Todos</button>
+        <button onClick={()=>setCategoty("users")}>Users</button>
+
       <button onClick={()=>setCount(count+1)}>+</button>
       <div>{count}</div>
       <button onClick={()=>setCount(count-1)}>-</button>
+      </div>
+
+    
+
+      <ul className="list">
+        <li className="list-item">item-1</li>
+      </ul>
+     
     </div>
   )
 }
